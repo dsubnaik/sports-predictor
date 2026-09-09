536d2e0072822884a8a0512080c37c37f314b69a
dsubnaik <derricksubnaik@gmail.com>
2026-09-08 23:02:28 -0500

    Add NFL odds event discovery

--- football/odds/__init__.py
+++ football/odds/__init__.py
@@ -19,6 +19,13 @@ from football.odds.event_matching import (
     ODDS_API_TEAM_TO_NFLVERSE,
     match_odds_events_to_schedule,
 )
+from football.odds.event_discovery import (
+    NFL_EVENT_COLUMNS,
+    NFL_EVENT_MATCH_OUTPUT_COLUMNS,
+    match_nfl_events_to_schedule,
+    normalize_nfl_events,
+    select_matched_nfl_events,
+)
 
 __all__ = [
     "PLAYER_PROP_ODDS_COLUMNS",
@@ -26,6 +33,8 @@ __all__ = [
     "PLAYER_PROP_MATCH_OUTPUT_COLUMNS",
     "EVENT_MATCH_COLUMNS",
     "EVENT_MATCH_OUTPUT_COLUMNS",
+    "NFL_EVENT_COLUMNS",
+    "NFL_EVENT_MATCH_OUTPUT_COLUMNS",
     "KICKOFF_TOLERANCE",
     "ODDS_API_TEAM_TO_NFLVERSE",
     "SUPPORTED_PLAYER_PROP_MARKETS",
@@ -34,4 +43,7 @@ __all__ = [
     "normalize_player_prop_odds",
     "match_player_prop_odds",
     "match_odds_events_to_schedule",
+    "match_nfl_events_to_schedule",
+    "normalize_nfl_events",
+    "select_matched_nfl_events",
 ]
